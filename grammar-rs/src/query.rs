5c64c8f3bd94de97ea80716e0e02a88efa65feb1
Giles Edkins <edkins@gmail.com>
2021-02-13 20:16:09 -0500

    Query work in progress, probably not needed

--- grammar-rs/src/query.rs
+++ grammar-rs/src/query.rs
@@ -13,8 +13,21 @@ pub enum Value {
 
 #[derive(Clone,Debug)]
 pub struct Query {
-    pub left: Form,
+    pub head: Label,
+    pub left: Vec<Value>,
     pub right: Value,
+}
+
+fn write_separated_values(f: &mut std::fmt::Formatter, xs: &[Value]) -> std::fmt::Result {
+    let mut first = true;
+    for x in xs {
+        if !first {
+            write!(f, ",")?;
+        }
+        first = false;
+        write!(f, "{}", x)?;
+    }
+    Ok(())
 }
 
 impl std::fmt::Display for Value {
@@ -24,14 +37,7 @@ impl std::fmt::Display for Value {
             Value::Str(s) => write!(f, "{}", s),
             Value::List(xs) => {
                 write!(f, "[")?;
-                let mut first = true;
-                for x in xs {
-                    if !first {
-                        write!(f, ",")?;
-                    }
-                    first = false;
-                    write!(f, "{}", x)?;
-                }
+                write_separated_values(f, xs)?;
                 write!(f, "]")?;
                 Ok(())
             }
@@ -41,7 +47,14 @@ impl std::fmt::Display for Value {
 
 impl std::fmt::Display for Query {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{} => {}", self.left, self.right)
+        write!(f, "{}", self.head)?;
+        if self.left.len() > 0 {
+            write!(f, "(")?;
+            write_separated_values(f, &self.left)?;
+            write!(f, ")")?;
+        }
+        write!(f, "=> {}", self.right)?;
+        Ok(())
     }
 }
 
@@ -63,60 +76,19 @@ impl Expr {
             _ => Err(SemanticError::NotAValue(format!("{}", self))),
         }
     }
-
-    // TODO: it would be nice not to have to duplicate all this code for the immutable case
-
-    fn to_head_tail_immut(&self, script: &Script) -> Result<(String,Vec<Form>), SemanticError> {
-        match self {
-            Expr::Call(w,_,es,_) => {
-                let tail = es.0.iter().map(|(e,_)|e.to_form_immut(script)).collect::<Result<_,_>>()?;
-                Ok((w.string.clone(),tail))
-            }
-            Expr::Word(w) => {
-                Ok((w.string.clone(),vec![]))
-            }
-            _ => Err(SemanticError::NotHeadTail(format!("{}", self)))
-        }
-    }
-
-    fn to_form_immut(&self, script: &Script) -> Result<Form,SemanticError> {
-        match self {
-            Expr::Num(n) => {
-                let head = Label::Num(n.n.clone());
-                Ok(Form{ head, tail: vec![] })
-            }
-            Expr::Call(_,_,_,_) | Expr::Word(_) => {
-                let (head,tail) = self.to_head_tail_immut(script)?;
-                match script.0.get(&head) {
-                    Some(DefinitionStatus::Terminal) => {
-                        if !tail.is_empty() { 
-                            return Err(SemanticError::ArityMismatch(head,0,tail.len()));
-                        }
-                    }
-                    Some(DefinitionStatus::Nonterminal(left,_)) | Some(DefinitionStatus::Builtin(left)) => {
-                        if tail.len() != left.len() {
-                            return Err(SemanticError::ArityMismatch(head,left.len(),tail.len()));
-                        }
-                    }
-                    None => return Err(SemanticError::Undefined(head))
-                }
-                let head = Label::Str(head);
-                Ok(Form{ head, tail })
-            }
-            Expr::HashWord(_,w) => Err(SemanticError::ScriptImmutable(w.string.clone())),
-            Expr::List(_,xs,_) => {
-                let tail = xs.0.iter().map(|(e,_)|e.to_form_immut(script)).collect::<Result<_,_>>()?;
-                Ok(Form{ head: Label::List, tail })
-            }
-        }
-    }
 }
 
 impl Expansion {
     pub fn to_query(&self, script: &Script) -> Result<Query, SemanticError> {
-        let left = self.left.to_form_immut(script)?;
+        let (head,left) = match &self.left {
+            Expr::Num(n) => (Label::Num(n.n.clone()), vec![]),
+            Expr::Call(f,_,xs,_) => (Label::Str(f.string.clone()), xs.0.iter().map(|(e,_)|e.to_value(script)).collect::<Result<_,_>>()?),
+            Expr::Word(x) => (Label::Str(x.string.clone()), vec![]),
+            Expr::HashWord(_,w) => return Err(SemanticError::ScriptImmutable(w.string.clone())),
+            Expr::List(_,xs,_) => (Label::List, xs.0.iter().map(|(e,_)|e.to_value(script)).collect::<Result<_,_>>()?),
+        };
         let right = self.right.to_value(script)?;
-        Ok(Query{left,right})
+        Ok(Query{head,left,right})
     }
 }
 
@@ -124,8 +96,8 @@ impl Expansion {
 pub enum QueryError {
     UnexpectedArityError,
     UnexpectedNotDefined,
+    Nondeterminism,
     NotImplementedBuiltin,
-    NotImplementedParam,
 }
 
 impl std::error::Error for QueryError {}
@@ -136,58 +108,70 @@ impl std::fmt::Display for QueryError {
     }
 }
 
+impl Value {
+    fn is_num(&self, n: &BigUint) -> bool {
+        match self {
+            Value::Num(n2) => n == n2,
+            _ => false
+        }
+    }
+    fn is_string(&self, s: &str) -> bool {
+        match self {
+            Value::Str(s2) => s == s2,
+            _ => false
+        }
+    }
+}
+
 impl Script {
     fn eval_query_and_add(&self, left: &Form, right: &Value, evals: &mut HashMap<Form,Value>) -> Result<bool, QueryError> {
         if self.eval_query(left, right, evals)? {
-            if let Some(x) = evals.get(left) {
-                Ok(x == right)
-            } else {
-                evals.insert(left.clone(), right.clone());
-                Ok(true)
+            match evals.get(left) {
+                Some(x) => Ok(x == right),
+                None => {
+                    evals.insert(left.clone(), right.clone());
+                    Ok(true)
+                }
             }
         } else {
             Ok(false)
         }
     }
-    fn eval_query(&self, left: &Form, right: &Value, evals: &mut HashMap<Form,Value>) -> Result<bool, QueryError> {
+    fn eval_form_and_add(&self, left: &Form, evals: &mut HashMap<Form,Value>) -> Result<Option<Value>, QueryError> {
+        if let Some(v) = evals.get(left) {
+            return Ok(Some(v.clone()));
+        }
+        if let Some(v) = self.eval_form(left, evals)? {
+            evals.insert(left.clone(), v);
+            Ok(Some(v))
+        } else {
+            Ok(None)
+        }
+    }
+    fn eval_form(&self, left: &Form, evals: &mut HashMap<Form,Value>) -> Result<Option<Value>, QueryError> {
         match &left.head {
             Label::Num(n) => {
                 if !left.tail.is_empty() {
                     return Err(QueryError::UnexpectedArityError);
                 }
-                match right {
-                    Value::Num(n2) => Ok(n == n2),
-                    _ => Ok(false)
-                }
+                Ok(Some(Value::Num(n.clone())))
             }
             Label::List => {
-                let xs = &left.tail;
-                match right {
-                    Value::List(ys) => {
-                        if xs.len() != ys.len () {
-                            return Ok(false);
-                        }
-                        for i in 0..xs.len() {
-                            if !self.eval_query_and_add(&xs[i], &ys[i], evals)? {
-                                return Ok(false);
-                            }
-                        }
-                        Ok(true)
+                let mut vs = vec![];
+                for x in &left.tail {
+                    if let Some(v) = self.eval_form_and_add(x, evals)? {
+                        vs.push(v);
+                    } else {
+                        return Ok(None);
                     }
-                    _ => Ok(false)
                 }
+                Ok(Some(Value::List(vs)))
             }
             Label::Str(s) => {
                 match self.0.get(s) {
                     None => Err(QueryError::UnexpectedNotDefined),
                     Some(DefinitionStatus::Terminal) => {
-                        if !left.tail.is_empty() {
-                            return Err(QueryError::UnexpectedArityError);
-                        }
-                        match right {
-                            Value::Str(s2) => Ok(s == s2),
-                            _ => Ok(false)
-                        }
+                        Ok(Some(Value::Str(s.clone())))
                     }
                     Some(DefinitionStatus::Builtin(_)) => {
                         Err(QueryError::NotImplementedBuiltin)
@@ -196,17 +180,27 @@ impl Script {
                         if left.tail.len() != nleft.len() {
                             return Err(QueryError::UnexpectedArityError);
                         }
-                        if left.tail.len() != 0 {
-                            // Parametric stuff is harder to implement
-                            return Err(QueryError::NotImplementedParam);
-                        }
-                        for nr in nright {
-                            let mut clone = evals.clone();
-                            if self.eval_query_and_add(nr, right, &mut clone)? {
-                                return Ok(true);
+                        let mut vs = vec![];
+                        for x in &left.tail {
+                            if let Some(v) = self.eval_form_and_add(x, evals)? {
+                                vs.push(v);
+                            } else {
+                                return Ok(None);
                             }
                         }
-                        Ok(false)    // false if we don't match any options
+
+                        let mut evals2 = HashMap::new();
+                        for i in 0..vs.len() {
+                            if !self.eval_query_and_add(&nleft[i], &vs[i], &mut evals2)? {
+                                return Ok(None);
+                            }
+                        }
+                        for nr in nright {
+                            if let Some(v) = self.eval_form_and_add(nr, &mut evals2.clone())? {
+                                return Ok(Some(v));
+                            }
+                        }
+                        Ok(None)    // None if we don't match any options
                     }
                 }
             }
@@ -216,7 +210,50 @@ impl Script {
 
 impl Query {
     pub fn eval(&self, script: &Script) -> Result<bool, QueryError> {
-        let mut evals = HashMap::new();
-        script.eval_query_and_add(&self.left, &self.right, &mut evals)
+        match &self.head {
+            Label::Num(n) => {
+                if !self.left.is_empty() {
+                    return Err(QueryError::UnexpectedArityError);
+                }
+                Ok(self.right.is_num(n))
+            }
+            Label::List => {
+                match &self.right {
+                    Value::List(xs) => Ok(&self.left == xs),
+                    _ => Ok(false)
+                }
+            }
+            Label::Str(s) => {
+                match script.0.get(s) {
+                    None => Err(QueryError::UnexpectedNotDefined),
+                    Some(DefinitionStatus::Terminal) => {
+                        if !self.left.is_empty() {
+                            return Err(QueryError::UnexpectedArityError);
+                        }
+                        Ok(self.right.is_string(s))
+                    }
+                    Some(DefinitionStatus::Builtin(_)) => {
+                        Err(QueryError::NotImplementedBuiltin)
+                    }
+                    Some(DefinitionStatus::Nonterminal(nleft, nright)) => {
+                        if self.left.len() != nleft.len() {
+                            return Err(QueryError::UnexpectedArityError);
+                        }
+                        let mut evals = HashMap::new();
+                        for i in 0..self.left.len() {
+                            if !script.eval_query_and_add(&nleft[i], &self.left[i], &mut evals)? {
+                                return Ok(false);
+                            }
+                        }
+                        for nr in nright {
+                            if script.eval_query_and_add(nr, &self.right, &mut evals.clone())? {
+                                return Ok(true);
+                            }
+                        }
+                        Ok(false)    // false if we don't match any options
+                    }
+                }
+            }
+        }
     }
 }
