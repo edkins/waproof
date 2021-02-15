7100a2e37eb9fa3a637342fe398974b37cd423f7
Giles Edkins <edkins@gmail.com>
2021-02-14 23:43:36 -0500

    Add functions and opaque types

--- subset-main/src/main.rs
+++ subset-main/src/main.rs
@@ -1,8 +1,18 @@
 mod standard;
 mod valid;
 
+use valid::FuncType;
+
 fn main() {
-    let ctx = standard::std_context();
+    let mut ctx = standard::std_context();
+    ctx.add_opaque_type("Foo", &[
+        ("Foo::four", FuncType{res:"Foo".to_owned(), args:vec![]}),
+        ("Foo::six", FuncType{res:"Foo".to_owned(), args:vec![]}),
+        ("Foo::to_nat", FuncType{res:"BigUint".to_owned(), args:vec!["Foo".to_owned()]}),
+    ]).unwrap();
     println!("{:?}", ctx.nat("BigUint",42u8.into()));
     println!("{:?}", ctx.structure("()", &[]));
+    let four = ctx.call("Foo::four", &[]).unwrap();
+    let four_n = ctx.call("Foo::to_nat", &[four]).unwrap();
+    println!("{:?}", four_n);
 }
--- subset-main/src/valid.rs
+++ subset-main/src/valid.rs
@@ -17,11 +17,13 @@ pub struct ValidContext{
 #[derive(Debug)]
 pub enum ValidationError {
     // Errors with a Rust compiler equivalent
+    E0061ArgCountMismatch(usize,usize),
     E0063MissingField(usize,usize),  // field names collapsed to integers, so this means too few fields supplied
     E0308MismatchedTypes(String,String),   // multiple places
     E0308MismatchInteger(String),
     E0412NotInScope(String),
     E0422NotInScopeStruct(String),
+    E0425NotInScopeFn(String),
     E0428Redefined(String),
     E0560DoesNotHaveThisField(usize,usize),  // field names collapsed to integers, so this means too many fields supplied
     E0574NotAStructType(String),
@@ -39,10 +41,12 @@ pub struct FuncType {
 pub enum TypeDef {
     Nat,
     Struct(Vec<String>),
+    Opaque,
 }
 
 #[derive(Clone,Debug,Eq,PartialEq)]
 pub enum Expr {
+    Call(String, Vec<Expr>),
     Nat(BigUint),
     Struct(String, Vec<Expr>),
 }
@@ -86,6 +90,35 @@ impl ValidContext {
         Ok(())
     }
 
+    pub fn add_opaque_type(&mut self, name: &str, funcs: &[(&str,FuncType)]) -> Result<(),ValidationError> {
+        if self.types.contains_key(name) {
+            return Err(ValidationError::E0428Redefined(name.to_owned()));
+        }
+        for i in 0..funcs.len() {
+            if self.funcs.contains_key(funcs[i].0) {
+                return Err(ValidationError::E0428Redefined(funcs[i].0.to_owned()));
+            }
+            for j in 0..i {
+                if funcs[i].0 == funcs[j].0 {
+                    return Err(ValidationError::E0428Redefined(funcs[i].0.to_owned()));
+                }
+            }
+            if funcs[i].1.res != name && !self.types.contains_key(&funcs[i].1.res) {
+                return Err(ValidationError::E0412NotInScope(funcs[i].1.res.clone()));
+            }
+            for arg in &funcs[i].1.args {
+                if arg != name && !self.types.contains_key(arg) {
+                    return Err(ValidationError::E0412NotInScope(arg.clone()));
+                }
+            }
+        }
+        self.types.insert(name.to_owned(), TypeDef::Opaque);
+        for func in funcs {
+            self.funcs.insert(func.0.to_owned(), func.1.clone());
+        }
+        Ok(())
+    }
+
     pub fn nat(&self, typ: &str, value: BigUint) -> Result<ValidExpr,ValidationError> {
         match self.types.get(typ) {
             Some(TypeDef::Nat) => {
@@ -124,6 +157,29 @@ impl ValidContext {
             context: self.clone(),
             typ: name.to_owned(),
             expr: Expr::Struct(name.to_owned(), fields.iter().map(|e|e.expr.clone()).collect()),
+        })
+    }
+
+    pub fn call(&self, name: &str, args: &[ValidExpr]) -> Result<ValidExpr,ValidationError> {
+        let func_def = match self.funcs.get(name) {
+            Some(fd) => fd,
+            None => return Err(ValidationError::E0425NotInScopeFn(name.to_owned())),
+        };
+        if args.len() != func_def.args.len() {
+            return Err(ValidationError::E0061ArgCountMismatch(func_def.args.len(), args.len()));
+        }
+        for i in 0..args.len() {
+            if args[i].typ != func_def.args[i] {
+                return Err(ValidationError::E0308MismatchedTypes(func_def.args[i].clone(), args[i].typ.clone()));
+            }
+            if args[i].context != *self {
+                return Err(ValidationError::MyMismatchedContext);
+            }
+        }
+        Ok(ValidExpr{
+            context: self.clone(),
+            typ: func_def.res.clone(),
+            expr: Expr::Call(name.to_owned(), args.iter().map(|e|e.expr.clone()).collect()),
         })
     }
 }
